98661377b3a0b608c9460333e76bd3cd4f127e33
Nitesh Turaga <nitesh.turaga@gmail.com>
2019-04-02 08:55:41 +0530

    Add volume to script, correct typo

--- src/run_devel.sh
+++ src/run_devel.sh
@@ -1,8 +1,9 @@
 # Run update
 sudo docker run -it \
      -v /home/nitesh_turaga_gmail_com/shared-devel:/usr/local/lib/R/host-site-library \
-     -v /home/nitesh_turaga_gmail_com/AnVIL_Docker/src/update_packages:/tmp/update_packages.R \
-     bioconductor/bioconductor_full:devel R -f /tmp/update_packages.R
+     -v /home/nitesh_turaga_gmail_com/AnVIL_Docker/src/update_packages.R:/tmp/update_packages.R \
+     bioconductor/bioconductor_full:devel \
+     R -f /tmp/update_packages.R
 
 # Run rsync
 gsutil -m rsync -d -r shared-devel/ gs://bioconductor-full-devel
