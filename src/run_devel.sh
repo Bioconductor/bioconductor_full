ce1ef97f13f11d5e21ec5e8cebc84ca3555a9211
Nitesh Turaga <nitesh.turaga@gmail.com>
2019-04-02 08:27:22 +0530

    Share volume to update_packages script

--- src/run_devel.sh
+++ src/run_devel.sh
@@ -1,7 +1,8 @@
 # Run update
 sudo docker run -it \
      -v /home/nitesh_turaga_gmail_com/shared-devel:/usr/local/lib/R/host-site-library \
-     bioconductor/bioconductor_full:devel R -f update_packages.R
+     -v /home/nitesh_turaga_gmail_com/AnVIL_Docker/src/update_packages:/tmp/update_packages.R \
+     bioconductor/bioconductor_full:devel R -f /tmp/update_packages.R
 
 # Run rsync
 gsutil -m rsync -d -r shared-devel/ gs://bioconductor-full-devel
