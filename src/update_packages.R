e079b47a82def457bc07ac249a8b472d6a2fc980
Nitesh Turaga <nitesh.turaga@gmail.com>
2019-08-09 16:17:22 -0400

    Install suggests section in the dependencies as well

--- src/update_packages.R
+++ src/update_packages.R
@@ -9,7 +9,17 @@ BiocManager::install(valid()$out_of_date, Ncpus=4)
 if (BiocManager:::isDevel()) {
     installed <- rownames(installed.packages())
     biocsoft <- available.packages(repos = BiocManager::repositories()[["BioCsoft"]])
+
     ## Packages which failed to install on docker image
     to_install <- rownames(biocsoft)[!rownames(biocsoft) %in% installed]
     BiocManager::install(to_install, Ncpus=4)
 }
+
+
+## Install suggests
+db <- available.packages(repos = BiocManager::repositories())
+pkgs <- tools::package_dependencies(rownames(biocsoft), db=db, which=c("Suggests"))
+suggests <- unique(unlist(pkgs, use.names=FALSE))
+
+to_install2 <- suggests[!suggests %in% rownames(installed.packages())]
+BiocManager::install(to_install2, destdir="/root/store",Ncpus=4)
